9b20df9afcb72a00d7a7abf5f79bc945280b54b1
Dania Mohamed <sc20dm@leeds.ac.uk>
2023-01-21 12:07:56 +0100

    Add using psql to create database

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -2,11 +2,12 @@
 const Sequelize = require("sequelize");
 
 // new sequelize instance with the specified database name, username, and password
+// if database not created use psql postgres to create one
 const sequelize = new Sequelize("jobs", "postgres", "sc20dm", {
    host: "localhost",
    port: 5432,
    dialect: "postgres",
-   logging: false, // disable logging of SQL queries to console
+   logging: false,
 });
 
 module.exports = sequelize;

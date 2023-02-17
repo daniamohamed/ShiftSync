e7287473cbde8c76a20f873499df8a062d0b9fe9
Dania Mohamed <sc20dm@leeds.ac.uk>
2023-02-17 18:56:19 +0100

    Updated sign up page

--- frontend/src/component/user_signup.js
+++ frontend/src/component/user_signup.js
@@ -111,3 +111,385 @@ const MultifieldInput = (props) => {
       </>
    );
 };
+
+// login front end
+const Login = (props) => {
+   const classes = useStyles();
+   const setPopup = useContext(SetPopupContext);
+
+   const [loggedin, setLoggedin] = useState(isAuth());
+
+   const [signupDetails, setSignupDetails] = useState({
+      type: "applicant",
+      email: "",
+      password: "",
+      name: "",
+      education: [],
+      skills: [],
+      bio: "",
+      contactNumber: "",
+   });
+
+   const [phone, setPhone] = useState("");
+
+   const [education, setEducation] = useState([
+      {
+         institutionName: "",
+         startYear: "",
+         endYear: "",
+      },
+   ]);
+
+   const [inputErrorHandler, setInputErrorHandler] = useState({
+      email: {
+         untouched: true,
+         required: true,
+         error: false,
+         message: "",
+      },
+      password: {
+         untouched: true,
+         required: true,
+         error: false,
+         message: "",
+      },
+      name: {
+         untouched: true,
+         required: true,
+         error: false,
+         message: "",
+      },
+   });
+
+   const handleInput = (key, value) => {
+      setSignupDetails({
+         ...signupDetails,
+         [key]: value,
+      });
+   };
+
+   const handleInputError = (key, status, message) => {
+      setInputErrorHandler({
+         ...inputErrorHandler,
+         [key]: {
+            required: true,
+            untouched: false,
+            error: status,
+            message: message,
+         },
+      });
+   };
+
+   const handleLogin = () => {
+      const tmpErrorHandler = {};
+      Object.keys(inputErrorHandler).forEach((obj) => {
+         if (
+            inputErrorHandler[obj].required &&
+            inputErrorHandler[obj].untouched
+         ) {
+            tmpErrorHandler[obj] = {
+               required: true,
+               untouched: false,
+               error: true,
+               message: `${obj[0].toUpperCase() + obj.substr(1)} is required`,
+            };
+         } else {
+            tmpErrorHandler[obj] = inputErrorHandler[obj];
+         }
+      });
+
+      education
+         .filter((obj) => obj.institutionName.trim() !== "")
+         .map((obj) => {
+            if (obj["endYear"] === "") {
+               delete obj["endYear"];
+            }
+            return obj;
+         });
+      for (var i = 0; i < education.length; i++) {
+         education[i].startYear = +education[i].startYear;
+         education[i].endYear = +education[i].endYear;
+      }
+
+      let updatedDetails = {
+         ...signupDetails,
+         education: education,
+      };
+
+      setSignupDetails(updatedDetails);
+
+      const verified = !Object.keys(tmpErrorHandler).some((obj) => {
+         return tmpErrorHandler[obj].error;
+      });
+
+      if (verified) {
+         axios
+            .post(apiList.signup, updatedDetails)
+            .then((response) => {
+               if (response && response.data) {
+                  localStorage.setItem("token", response.data.token);
+                  localStorage.setItem("type", response.data.type);
+                  setLoggedin(isAuth());
+                  setPopup({
+                     open: true,
+                     severity: "success",
+                     message: "Logged in successfully",
+                  });
+                  console.log(response);
+               } else {
+                  // handle the case where response or response.data is undefined
+                  setPopup({
+                     open: true,
+                     severity: "error",
+                     message:
+                        "An error occurred while logging in. Please try again later.",
+                  });
+               }
+            })
+            .catch((err) => {
+               if (err.response && err.response.data) {
+                  setPopup({
+                     open: true,
+                     severity: "error",
+                     message: err.response.data.message,
+                  });
+                  console.log(err.response);
+               } else {
+                  // handle the case where err.response or err.response.data is undefined
+                  setPopup({
+                     open: true,
+                     severity: "error",
+                     message:
+                        "An error occurred while logging in. Please try again later.",
+                  });
+               }
+            });
+      } else {
+         setInputErrorHandler(tmpErrorHandler);
+         setPopup({
+            open: true,
+            severity: "error",
+            message: "Incorrect Input",
+         });
+      }
+   };
+
+   const handleLoginRecruiter = () => {
+      const tmpErrorHandler = {};
+      Object.keys(inputErrorHandler).forEach((obj) => {
+         if (
+            inputErrorHandler[obj].required &&
+            inputErrorHandler[obj].untouched
+         ) {
+            tmpErrorHandler[obj] = {
+               required: true,
+               untouched: false,
+               error: true,
+               message: `${obj[0].toUpperCase() + obj.substr(1)} is required`,
+            };
+         } else {
+            tmpErrorHandler[obj] = inputErrorHandler[obj];
+         }
+      });
+
+      let updatedDetails = {
+         ...signupDetails,
+      };
+      if (phone !== "") {
+         updatedDetails = {
+            ...signupDetails,
+            contactNumber: `+${phone}`,
+         };
+      } else {
+         updatedDetails = {
+            ...signupDetails,
+            contactNumber: "",
+         };
+      }
+
+      setSignupDetails(updatedDetails);
+
+      const verified = !Object.keys(tmpErrorHandler).some((obj) => {
+         return tmpErrorHandler[obj].error;
+      });
+
+      if (verified) {
+         axios
+            .post(apiList.signup, updatedDetails)
+            .then((response) => {
+               localStorage.setItem("token", response.data.token);
+               localStorage.setItem("type", response.data.type);
+               setLoggedin(isAuth());
+               setPopup({
+                  open: true,
+                  severity: "success",
+                  message: "Logged in successfully",
+               });
+               console.log(response);
+            })
+            .catch((err) => {
+               setPopup({
+                  open: true,
+                  severity: "error",
+                  message: err.response.data.message,
+               });
+               console.log(err.response);
+            });
+      } else {
+         setInputErrorHandler(tmpErrorHandler);
+         setPopup({
+            open: true,
+            severity: "error",
+            message: "Incorrect Input",
+         });
+      }
+   };
+
+   return loggedin ? (
+      <Navigate to="/" />
+   ) : (
+      <Paper elevation={3} className={classes.body}>
+         <Grid container direction="column" spacing={3} alignItems="center">
+            <Grid item>
+               <Typography variant="h3" component="h2">
+                  Sign Up
+               </Typography>
+            </Grid>
+            <Grid item>
+               <TextField
+                  select
+                  label="Category"
+                  variant="outlined"
+                  className={classes.inputBox}
+                  value={signupDetails.type}
+                  onChange={(event) => {
+                     handleInput("type", event.target.value);
+                  }}
+               >
+                  <MenuItem value="applicant">Applicant</MenuItem>
+                  <MenuItem value="recruiter">Recruiter</MenuItem>
+               </TextField>
+            </Grid>
+            <Grid item>
+               <TextField
+                  label="Name"
+                  value={signupDetails.name}
+                  onChange={(event) => handleInput("name", event.target.value)}
+                  className={classes.inputBox}
+                  error={inputErrorHandler.name.error}
+                  helperText={inputErrorHandler.name.message}
+                  onBlur={(event) => {
+                     if (event.target.value === "") {
+                        handleInputError("name", true, "Name is required");
+                     } else {
+                        handleInputError("name", false, "");
+                     }
+                  }}
+                  variant="outlined"
+               />
+            </Grid>
+            <Grid item>
+               <EmailInput
+                  label="Email"
+                  value={signupDetails.email}
+                  onChange={(event) => handleInput("email", event.target.value)}
+                  inputErrorHandler={inputErrorHandler}
+                  handleInputError={handleInputError}
+                  className={classes.inputBox}
+                  required={true}
+               />
+            </Grid>
+            <Grid item>
+               <PasswordInput
+                  label="Password"
+                  value={signupDetails.password}
+                  onChange={(event) =>
+                     handleInput("password", event.target.value)
+                  }
+                  className={classes.inputBox}
+                  error={inputErrorHandler.password.error}
+                  helperText={inputErrorHandler.password.message}
+                  onBlur={(event) => {
+                     if (event.target.value === "") {
+                        handleInputError(
+                           "password",
+                           true,
+                           "Password is required"
+                        );
+                     } else {
+                        handleInputError("password", false, "");
+                     }
+                  }}
+               />
+            </Grid>
+            {signupDetails.type === "applicant" ? (
+               <>
+                  <MultifieldInput
+                     education={education}
+                     setEducation={setEducation}
+                  />
+                  <Grid item>
+                     <ChipInput
+                        className={classes.inputBox}
+                        label="Skills"
+                        variant="outlined"
+                        helperText="Press enter to add skills"
+                        onChange={(chips) =>
+                           setSignupDetails({ ...signupDetails, skills: chips })
+                        }
+                     />
+                  </Grid>
+               </>
+            ) : (
+               <>
+                  <Grid item style={{ width: "100%" }}>
+                     <TextField
+                        label="Bio (upto 250 words)"
+                        multiline
+                        minRows={8}
+                        style={{ width: "100%" }}
+                        variant="outlined"
+                        value={signupDetails.bio}
+                        onChange={(event) => {
+                           if (
+                              event.target.value
+                                 .split(" ")
+                                 .filter(function (n) {
+                                    return n !== "";
+                                 }).length <= 250
+                           ) {
+                              handleInput("bio", event.target.value);
+                           }
+                        }}
+                     />
+                  </Grid>
+                  <Grid item>
+                     <PhoneInput
+                        country={"gb"}
+                        value={phone}
+                        onChange={(phone) => setPhone(phone)}
+                     />
+                  </Grid>
+               </>
+            )}
+
+            <Grid item>
+               <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={() => {
+                     signupDetails.type === "applicant"
+                        ? handleLogin()
+                        : handleLoginRecruiter();
+                  }}
+                  className={classes.submitButton}
+               >
+                  Signup
+               </Button>
+            </Grid>
+         </Grid>
+      </Paper>
+   );
+};
+
+export default Login;

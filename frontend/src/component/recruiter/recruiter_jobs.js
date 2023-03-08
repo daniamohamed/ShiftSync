2dd7fbc680c351b26cb1bce3a2c2043226ffc16f
Dania Mohamed <sc20dm@leeds.ac.uk>
2023-03-08 23:08:36 +0100

    Updated recruiters jobs page

--- frontend/src/component/recruiter/recruiter_jobs.js
+++ frontend/src/component/recruiter/recruiter_jobs.js
@@ -130,6 +130,217 @@ const JobTile = (props) => {
             handleCloseUpdate();
          });
    };
+
+   const postedOn = new Date(job.dateOfPosting);
+
+   return (
+      <Paper className={classes.jobTileOuter} elevation={3}>
+         <Grid container>
+            <Grid container item xs={9} spacing={1} direction="column">
+               <Grid item>
+                  <Typography variant="h5">{job.title}</Typography>
+               </Grid>
+               <Grid item>
+                  <Rating
+                     value={job.rating !== -1 ? job.rating : null}
+                     readOnly
+                  />
+               </Grid>
+               <Grid item>Role : {job.jobType}</Grid>
+               <Grid item>Salary : &#163; {job.salary} per hour</Grid>
+               <Grid item>
+                  Duration :{" "}
+                  {job.duration !== 0 ? `${job.duration} hours` : `Flexible`}
+               </Grid>
+               <Grid item>
+                  Date Of Posting: {postedOn.toLocaleDateString()}
+               </Grid>
+               <Grid item>
+                  Remaining Number of Positions:{" "}
+                  {job.maxPositions - job.acceptedCandidates}
+               </Grid>
+               <Grid item>
+                  {job.skillsets.map((skill) => (
+                     <Chip label={skill} style={{ marginRight: "2px" }} />
+                  ))}
+               </Grid>
+            </Grid>
+            <Grid item container direction="column" xs={3}>
+               <Grid item xs>
+                  <Button
+                     variant="contained"
+                     color="primary"
+                     className={classes.statusBlock}
+                     onClick={() => handleClick(`/job/applications/${job.jid}`)}
+                  >
+                     View Applications
+                  </Button>
+               </Grid>
+               <Grid item>
+                  <Button
+                     variant="contained"
+                     className={classes.statusBlock}
+                     onClick={() => {
+                        setOpenUpdate(true);
+                     }}
+                     style={{
+                        background: "#FC7A1E",
+                        color: "#fff",
+                     }}
+                  >
+                     Update Details
+                  </Button>
+               </Grid>
+               <Grid item>
+                  <Button
+                     variant="contained"
+                     color="secondary"
+                     className={classes.statusBlock}
+                     onClick={() => {
+                        setOpen(true);
+                     }}
+                  >
+                     Delete Job
+                  </Button>
+               </Grid>
+            </Grid>
+         </Grid>
+         <Modal
+            open={open}
+            onClose={handleClose}
+            className={classes.popupDialog}
+         >
+            <Paper
+               style={{
+                  padding: "20px",
+                  outline: "none",
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  minWidth: "30%",
+                  alignItems: "center",
+               }}
+            >
+               <Typography variant="h4" style={{ marginBottom: "10px" }}>
+                  Are you sure?
+               </Typography>
+               <Grid container justify="center" spacing={5}>
+                  <Grid item>
+                     <Button
+                        variant="contained"
+                        color="secondary"
+                        style={{ padding: "10px 50px" }}
+                        onClick={() => handleDelete()}
+                     >
+                        Delete
+                     </Button>
+                  </Grid>
+                  <Grid item>
+                     <Button
+                        variant="contained"
+                        color="primary"
+                        style={{ padding: "10px 50px" }}
+                        onClick={() => handleClose()}
+                     >
+                        Cancel
+                     </Button>
+                  </Grid>
+               </Grid>
+            </Paper>
+         </Modal>
+         <Modal
+            open={openUpdate}
+            onClose={handleCloseUpdate}
+            className={classes.popupDialog}
+         >
+            <Paper
+               style={{
+                  padding: "20px",
+                  outline: "none",
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  minWidth: "30%",
+                  alignItems: "center",
+               }}
+            >
+               <Typography variant="h4" style={{ marginBottom: "10px" }}>
+                  Update Details
+               </Typography>
+               <Grid
+                  container
+                  direction="column"
+                  spacing={3}
+                  style={{ margin: "10px" }}
+               >
+                  <Grid item>
+                     <TextField
+                        label="Application Deadline"
+                        type="datetime-local"
+                        value={jobDetails.deadline.substr(0, 16)}
+                        onChange={(event) => {
+                           handleInput("deadline", event.target.value);
+                        }}
+                        InputLabelProps={{
+                           shrink: true,
+                        }}
+                        variant="outlined"
+                        fullWidth
+                     />
+                  </Grid>
+                  <Grid item>
+                     <TextField
+                        label="Maximum Number Of Applicants"
+                        type="number"
+                        variant="outlined"
+                        value={jobDetails.maxApplicants}
+                        onChange={(event) => {
+                           handleInput("maxApplicants", event.target.value);
+                        }}
+                        InputProps={{ inputProps: { min: 1 } }}
+                        fullWidth
+                     />
+                  </Grid>
+                  <Grid item>
+                     <TextField
+                        label="Positions Available"
+                        type="number"
+                        variant="outlined"
+                        value={jobDetails.maxPositions}
+                        onChange={(event) => {
+                           handleInput("maxPositions", event.target.value);
+                        }}
+                        InputProps={{ inputProps: { min: 1 } }}
+                        fullWidth
+                     />
+                  </Grid>
+               </Grid>
+               <Grid container justifyContent="center" spacing={5}>
+                  <Grid item>
+                     <Button
+                        variant="contained"
+                        color="secondary"
+                        style={{ padding: "10px 50px" }}
+                        onClick={() => handleJobUpdate()}
+                     >
+                        Update
+                     </Button>
+                  </Grid>
+                  <Grid item>
+                     <Button
+                        variant="contained"
+                        color="primary"
+                        style={{ padding: "10px 50px" }}
+                        onClick={() => handleCloseUpdate()}
+                     >
+                        Cancel
+                     </Button>
+                  </Grid>
+               </Grid>
+            </Paper>
+         </Modal>
+      </Paper>
+   );
 };
 
 const MyJobs = (props) => {
@@ -234,6 +445,49 @@ const MyJobs = (props) => {
             });
          });
    };
+
+   return (
+      <>
+         <Grid
+            container
+            item
+            direction="column"
+            alignItems="center"
+            style={{ padding: "30px", minHeight: "93vh" }}
+         >
+            <Grid
+               item
+               container
+               direction="column"
+               justifyContent="center"
+               alignItems="center"
+            >
+               <Grid item xs>
+                  <Typography variant="h2">My Jobs</Typography>
+               </Grid>
+            </Grid>
+
+            <Grid
+               container
+               item
+               xs
+               direction="column"
+               alignItems="stretch"
+               justifyContent="center"
+            >
+               {jobs.length > 0 ? (
+                  jobs.map((job) => {
+                     return <JobTile job={job} getData={getData} />;
+                  })
+               ) : (
+                  <Typography variant="h5" style={{ textAlign: "center" }}>
+                     No jobs found
+                  </Typography>
+               )}
+            </Grid>
+         </Grid>
+      </>
+   );
 };
 
 export default MyJobs;

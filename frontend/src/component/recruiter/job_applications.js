f580ea13b47b99ee6dd0a8775383602086146fcc
Dania Mohamed <sc20dm@leeds.ac.uk>
2023-03-20 08:16:04 +0100

    Updated applications page

--- frontend/src/component/recruiter/job_applications.js
+++ frontend/src/component/recruiter/job_applications.js
@@ -368,6 +368,387 @@ const FilterPopup = (props) => {
    );
 };
 
+const ApplicationTile = (props) => {
+   const classes = useStyles();
+   const { application, getData } = props;
+   const setPopup = useContext(SetPopupContext);
+   const [open, setOpen] = useState(false);
+
+   const appliedOn = new Date(application.dateOfApplication);
+
+   const handleClose = () => {
+      setOpen(false);
+   };
+
+   const colorSet = {
+      applied: "#3454D1",
+      shortlisted: "#DC851F",
+      accepted: "#09BC8A",
+      rejected: "#D1345B",
+      deleted: "#B49A67",
+      cancelled: "#FF8484",
+      finished: "#4EA5D9",
+   };
+
+   const getResume = () => {
+      if (
+         application.Jobapplicant.resume &&
+         application.Jobapplicant.resume !== ""
+      ) {
+         const address = server + application.Jobapplicant.resume;
+         axios(address, {
+            method: "GET",
+            responseType: "blob",
+         })
+            .then((response) => {
+               const file = new Blob([response.data], {
+                  type: "application/pdf",
+               });
+               const fileURL = URL.createObjectURL(file);
+               window.open(fileURL);
+            })
+            .catch((error) => {
+               setPopup({
+                  open: true,
+                  severity: "error",
+                  message: "Error",
+               });
+            });
+      } else {
+         setPopup({
+            open: true,
+            severity: "error",
+            message: "No resume found",
+         });
+      }
+   };
+
+   const updateStatus = (status) => {
+      const address = `${apiList.applications}/${application.applicationId}`;
+      const statusData = {
+         status: status,
+         dateOfJoining: new Date().toISOString(),
+      };
+      axios
+         .put(address, statusData, {
+            headers: {
+               Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+         })
+         .then((response) => {
+            setPopup({
+               open: true,
+               severity: "success",
+               message: response.data.message,
+            });
+            getData();
+         })
+         .catch((err) => {
+            setPopup({
+               open: true,
+               severity: "error",
+               message: err.response.data.message,
+            });
+            console.log(err.response);
+         });
+   };
+
+   const buttonSet = {
+      applied: (
+         <>
+            <Grid item xs>
+               <Button
+                  className={classes.statusBlock}
+                  style={{
+                     background: colorSet["shortlisted"],
+                     color: "#ffffff",
+                  }}
+                  onClick={() => updateStatus("shortlisted")}
+               >
+                  Shortlist
+               </Button>
+            </Grid>
+            <Grid item xs>
+               <Button
+                  className={classes.statusBlock}
+                  style={{
+                     background: colorSet["rejected"],
+                     color: "#ffffff",
+                  }}
+                  onClick={() => updateStatus("rejected")}
+               >
+                  Reject
+               </Button>
+            </Grid>
+         </>
+      ),
+      shortlisted: (
+         <>
+            <Grid item xs>
+               <Button
+                  className={classes.statusBlock}
+                  style={{
+                     background: colorSet["accepted"],
+                     color: "#ffffff",
+                  }}
+                  onClick={() => updateStatus("accepted")}
+               >
+                  Accept
+               </Button>
+            </Grid>
+            <Grid item xs>
+               <Button
+                  className={classes.statusBlock}
+                  style={{
+                     background: colorSet["rejected"],
+                     color: "#ffffff",
+                  }}
+                  onClick={() => updateStatus("rejected")}
+               >
+                  Reject
+               </Button>
+            </Grid>
+         </>
+      ),
+      rejected: (
+         <>
+            <Grid item xs>
+               <Paper
+                  className={classes.statusBlock}
+                  style={{
+                     background: colorSet["rejected"],
+                     color: "#ffffff",
+                  }}
+               >
+                  Rejected
+               </Paper>
+            </Grid>
+         </>
+      ),
+      accepted: (
+         <>
+            <Grid item xs>
+               <Paper
+                  className={classes.statusBlock}
+                  style={{
+                     background: colorSet["accepted"],
+                     color: "#ffffff",
+                  }}
+               >
+                  Accepted
+               </Paper>
+            </Grid>
+         </>
+      ),
+      cancelled: (
+         <>
+            <Grid item xs>
+               <Paper
+                  className={classes.statusBlock}
+                  style={{
+                     background: colorSet["cancelled"],
+                     color: "#ffffff",
+                  }}
+               >
+                  Cancelled
+               </Paper>
+            </Grid>
+         </>
+      ),
+      finished: (
+         <>
+            <Grid item xs>
+               <Paper
+                  className={classes.statusBlock}
+                  style={{
+                     background: colorSet["finished"],
+                     color: "#ffffff",
+                  }}
+               >
+                  Finished
+               </Paper>
+            </Grid>
+         </>
+      ),
+   };
+
+   return (
+      <Paper className={classes.jobTileOuter} elevation={3}>
+         <Grid container>
+            <Grid
+               item
+               xs={2}
+               style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+               }}
+            >
+               <Avatar
+                  src={`${server}${application.Jobapplicant.profile}`}
+                  style={{ width: "70px", height: "70px" }}
+                  className={classes.avatar}
+               />
+            </Grid>
+            <Grid container item xs={7} spacing={1} direction="column">
+               <Grid item>
+                  <Typography variant="h5">
+                     {application.Jobapplicant.name}
+                  </Typography>
+               </Grid>
+               <Grid item>
+                  <Rating
+                     value={
+                        application.Jobapplicant.rating !== -1
+                           ? application.Jobapplicant.rating
+                           : null
+                     }
+                     readOnly
+                  />
+               </Grid>
+               <Grid item>Applied On: {appliedOn.toLocaleDateString()}</Grid>
+               <Grid item>
+                  Education:{" "}
+                  {application.Jobapplicant.education
+                     .map((edu) => {
+                        return `${edu.institutionName} (${edu.startYear}-${
+                           edu.endYear ? edu.endYear : "Ongoing"
+                        })`;
+                     })
+                     .join(", ")}
+               </Grid>
+               <Grid item>
+                  Cover Letter:{" "}
+                  {application.sop !== "" ? application.sop : "Not Submitted"}
+               </Grid>
+               <Grid item>
+                  {application.Jobapplicant.skills.map((skill) => (
+                     <Chip label={skill} style={{ marginRight: "2px" }} />
+                  ))}
+               </Grid>
+            </Grid>
+            <Grid item container direction="column" xs={3}>
+               <Grid item container xs>
+                  {buttonSet[application.status]}
+               </Grid>
+            </Grid>
+         </Grid>
+         <Modal
+            open={open}
+            onClose={handleClose}
+            className={classes.popupDialog}
+         >
+            <Paper
+               style={{
+                  padding: "20px",
+                  outline: "none",
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  minWidth: "30%",
+                  alignItems: "center",
+               }}
+            >
+               <Button
+                  variant="contained"
+                  color="primary"
+                  style={{ padding: "10px 50px" }}
+               >
+                  Submit
+               </Button>
+            </Paper>
+         </Modal>
+      </Paper>
+   );
+};
+
+const JobApplications = (props) => {
+   const setPopup = useContext(SetPopupContext);
+   const [applications, setApplications] = useState([]);
+   const { jobId } = useParams();
+   const [filterOpen, setFilterOpen] = useState(false);
+   const [searchOptions, setSearchOptions] = useState({
+      status: {
+         all: false,
+         applied: false,
+         shortlisted: false,
+      },
+      sort: {
+         "Jobapplicant.name": {
+            status: false,
+            desc: false,
+         },
+         dateOfApplication: {
+            status: true,
+            desc: true,
+         },
+         "Jobapplicant.rating": {
+            status: false,
+            desc: false,
+         },
+      },
+   });
+
+   useEffect(() => {
+      getData();
+   }, []);
+
+   const getData = () => {
+      let searchParams = [];
+
+      if (searchOptions.status.rejected) {
+         searchParams = [...searchParams, `status=rejected`];
+      }
+      if (searchOptions.status.applied) {
+         searchParams = [...searchParams, `status=applied`];
+      }
+      if (searchOptions.status.shortlisted) {
+         searchParams = [...searchParams, `status=shortlisted`];
+      }
+
+      let asc = [],
+         desc = [];
+
+      Object.keys(searchOptions.sort).forEach((obj) => {
+         const item = searchOptions.sort[obj];
+         if (item.status) {
+            if (item.desc) {
+               desc = [...desc, `desc=${obj}`];
+            } else {
+               asc = [...asc, `asc=${obj}`];
+            }
+         }
+      });
+      searchParams = [...searchParams, ...asc, ...desc];
+      const queryString = searchParams.join("&");
+      console.log("QueryString:", queryString);
+      let address = `${apiList.applicants}?jobId=${jobId}`;
+      if (queryString !== "") {
+         address = `${address}&${queryString}`;
+      }
+
+      axios
+         .get(address, {
+            headers: {
+               Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+         })
+         .then((response) => {
+            console.log(response.data);
+            setApplications(response.data);
+         })
+         .catch((err) => {
+            console.log(err);
+            // console.log(err.response.data);
+            setApplications([]);
+            setPopup({
+               open: true,
+               severity: "error",
+               message: err.response.data.message,
+            });
+         });
+   };
+
    return (
       <>
          <Grid
